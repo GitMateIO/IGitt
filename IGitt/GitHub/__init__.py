0a0aaa770bda3ef7fe1f370b907ce2b8360abc81
Tushar Gautam <tushar.rishav@gmail.com>
2016-03-10 16:34:39 +0530

    Github/__init__.py: Add pagination in request
    Previously, get method used to fetch partial results. Link
    header were completely ignored. Following the Link headers
    this commit fetches data completely.
    
    Fixes https://gitlab.com/gitmate/IGitt/issues/8

--- IGitt/GitHub/__init__.py
+++ IGitt/GitHub/__init__.py
@@ -3,14 +3,50 @@ This package contains the GitHub implementations of the interfaces in
 server.git.Interfaces.
 """
 
-from json import loads
-
-from requests import delete as raw_delete
-from requests import get as raw_get
-from requests import patch as raw_patch
-from requests import post as raw_post
+from requests import Session
 
 HEADERS = {'User-Agent': 'GitMate'}
+BASE_URL = "https://api.github.com"
+
+
+def _fetch_all(req_type: str, token: str, url: str, data: dict=None):
+    """
+    Fetch all the contents by following
+    the ``Link`` header.
+
+    :param req_type: A request type. Get, Post, Patch and Delete.
+    :param token: An OAuth token.
+    :param url  : E.g. ``/repo``
+    :param data : The data to post. Used for Patch and Post methods only
+    :return     : A dictionary or a list of dictionary if the response contains
+                  multiple items ( usually in case of pagination )
+    """
+
+    data_container = []
+    req = Session()
+    req_methods = {
+        'get': req.get,
+        'post': req.post,
+        'patch': req.patch,
+        'delete': req.delete}
+    params = {'access_token': token}
+    req.params.update(params)
+    req.headers.update(HEADERS)
+    fetch_method = req_methods[req_type]
+    resp = fetch_method(BASE_URL + url, json=data)
+
+    # Delete request returns no response
+    if not len(resp.text):
+        return
+    if isinstance(resp.json(), dict):
+        return resp.json()
+    while resp.links.get('next', False):
+        data_container.extend(resp.json())
+        resp = fetch_method(resp.links.get('next')['url'], json=data)
+
+    # Add the last node data
+    data_container.extend(resp.json())
+    return data_container
 
 
 def get(token: str, url: str):
@@ -21,9 +57,7 @@ def get(token: str, url: str):
     :param url: E.g. ``/repo``
     :return: A dictionary with the data.
     """
-    return loads(raw_get('https://api.github.com' + url,
-                         params={'access_token': token},
-                         headers=HEADERS).text)
+    return _fetch_all('get', token, url)
 
 
 def post(token: str, url: str, data: dict):
@@ -35,9 +69,7 @@ def post(token: str, url: str, data: dict):
     :param data: The data to post.
     :return: The response as a dictionary.
     """
-    return loads(raw_post('https://api.github.com' + url,
-                          params={'access_token': token}, headers=HEADERS,
-                          json=data).text)
+    return _fetch_all('post', token, url, data)
 
 
 def patch(token: str, url: str, data: dict):
@@ -49,9 +81,7 @@ def patch(token: str, url: str, data: dict):
     :param data: The data to post.
     :return: The response as a dictionary.
     """
-    return loads(raw_patch('https://api.github.com' + url,
-                           params={'access_token': token}, headers=HEADERS,
-                           json=data).text)
+    return _fetch_all('patch', token, url, data)
 
 
 def delete(token: str, url: str):
@@ -61,5 +91,4 @@ def delete(token: str, url: str):
     :param token: An OAuth token.
     :param url: The URL to access, e.g. ``/repo``.
     """
-    raw_delete('https://api.github.com' + url, params={'access_token': token},
-               headers=HEADERS)
+    _ = _fetch_all('delete', token, url)
