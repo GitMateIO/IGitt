20f3e30a858b0826eed6cf2072d423a2be9588d1
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-06-07 14:31:02 +0530

    test_github_hoster: Add tests for GitHub
    Adds to https://gitlab.com/gitmate/open-source/IGitt/issues/34

---
+++ tests/GitHub/test_github_hoster.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+
+import vcr
+
+from IGitt.GitHub.GitHub import GitHub
+
+my_vcr = vcr.VCR(match_on=['method', 'scheme', 'host', 'port', 'path'],
+                 filter_query_parameters=['access_token'],
+                 filter_post_data_parameters=['access_token'])
+
+
+class TestGitHub(unittest.TestCase):
+
+    @my_vcr.use_cassette('tests/GitHub/cassettes/test_github_hoster.yaml')
+    def setUp(self):
+        self.gh = GitHub(os.environ.get('GITHUB_TEST_TOKEN', ''))
+
+    @my_vcr.use_cassette('tests/GitHub/cassettes/test_github_hoster_owned.yaml')
+    def test_owned_repositories(self):
+        self.assertEqual(sorted(map(lambda x: x.full_name, self.gh.owned_repositories)),
+                         ['gitmate-test-user/test'])
+
+    @my_vcr.use_cassette('tests/GitHub/cassettes/test_github_hoster_write.yaml')
+    def test_write_repositories(self):
+        self.assertEqual(sorted(map(lambda x: x.full_name, self.gh.write_repositories)),
+                         ['gitmate-test-user/test', 'sils/gitmate-test'])
+
+    @my_vcr.use_cassette('tests/GitHub/cassettes/test_github_hoster_get_repo.yaml')
+    def test_get_repo(self):
+        self.assertEqual(self.gh.get_repo('gitmate-test-user/test').full_name,
+                         'gitmate-test-user/test')
