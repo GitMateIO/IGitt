718603aa2294a55206cfaa8806aa624791757ec4
Hemang Kumar <hemangsk@gmail.com>
2017-07-16 20:04:49 +0530

    GitHub: Add put method

--- IGitt/GitHub/__init__.py
+++ IGitt/GitHub/__init__.py
@@ -98,3 +98,19 @@ def delete(token: GitHubToken, url: str, params: dict=None):
     """
     _fetch(BASE_URL, 'delete', token.parameter,
            url, params)
+
+
+def put(token: GitHubToken, url: str, data: dict):
+    """
+    Puts the given data onto GitHub.
+
+    :param token: An OAuth token.
+    :param url: The URL to access, e.g. ``/repo``.
+    :param data: The data to post.
+    :return:
+        A dictionary or a list of dictionary if the response contains multiple
+        items (usually in case of pagination) and the HTTP status code.
+    :raises RunTimeError:
+        If the response indicates any problem.
+    """
+    return _fetch(BASE_URL, 'put', token.parameter, url, data)
