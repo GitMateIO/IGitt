854c1dfd9a22b0d92e883d33e376d0f63566e8d7
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-07-31 13:07:16 +0200

    GitHub: Add handle_webhook

--- IGitt/GitHub/GitHub.py
+++ IGitt/GitHub/GitHub.py
@@ -3,6 +3,13 @@ Contains the Hoster implementation for GitHub.
 """
 
 from IGitt.GitHub import get, GitHubToken
+from IGitt.GitHub.GitHubComment import GitHubComment
+from IGitt.GitHub.GitHubCommit import GitHubCommit
+from IGitt.GitHub.GitHubIssue import GitHubIssue
+from IGitt.GitHub.GitHubMergeRequest import GitHubMergeRequest
+from IGitt.Interfaces.Actions import IssueActions, MergeRequestActions, \
+    PipelineActions
+from IGitt.Interfaces.Comment import CommentType
 from IGitt.Interfaces.Hoster import Hoster
 from IGitt.GitHub.GitHubRepository import GitHubRepository
 
@@ -74,3 +81,71 @@ class GitHub(Hoster):
         :return: A repository object.
         """
         return GitHubRepository(self._token, repository)
+
+    def handle_webhook(self, event: str, data: dict):
+        """
+        Handles a GitHub webhook for you.
+
+        If it's an issue event it returns e.g.
+        ``IssueActions.OPENED, [GitHubIssue(...)]``, for comments it returns
+        ``MergeRequestActions.COMMENTED,
+        [GitHubMergeRequest(...), GitHubComment(...)]``.
+
+        :param event: The HTTP_X_GITHUB_EVENT of the request header.
+        :param data:  The pythonified JSON data of the request.
+        :return:      An IssueActions or MergeRequestActions member and a list
+                      of the affected IGitt objects.
+        """
+        repository = data['repository']
+
+        if event == 'issues':
+            issue = data['issue']
+            issue_obj = GitHubIssue(
+                self._token, repository['full_name'], issue['number'])
+            trigger_event = {
+                'opened': IssueActions.OPENED,
+                'closed': IssueActions.CLOSED,
+                'reopened': IssueActions.REOPENED,
+            }.get(data['action'], IssueActions.ATTRIBUTES_CHANGED)
+
+            return trigger_event, [issue_obj]
+
+        if event == 'pull_request':
+            pull_request = data['pull_request']
+            pull_request_obj = GitHubMergeRequest(
+                self._token, repository['full_name'], pull_request['number'])
+            trigger_event = {
+                'synchronize': MergeRequestActions.SYNCHRONIZED,
+                'opened': MergeRequestActions.OPENED,
+            }.get(data['action'], MergeRequestActions.ATTRIBUTES_CHANGED)
+
+            return trigger_event, [pull_request_obj]
+
+        if event == 'issue_comment':
+            if data['action'] != 'deleted':
+                comment_obj = GitHubComment(
+                    self._token,
+                    repository['full_name'],
+                    CommentType.MERGE_REQUEST,
+                    data['comment']['id'])
+
+                if 'pull_request' in data['issue']:
+                    return MergeRequestActions.COMMENTED, [GitHubMergeRequest(
+                        self._token,
+                        repository['full_name'],
+                        data['issue']['number']
+                    ), comment_obj]
+
+                return IssueActions.COMMENTED, [GitHubIssue(
+                    self._token,
+                    repository['full_name'],
+                    data['issue']['number']
+                ), comment_obj]
+
+        if event == 'status':
+            commit = data['commit']
+            commit_obj = GitHubCommit(self._token, repository['full_name'],
+                                      commit['sha'])
+            return PipelineActions.UPDATED, [commit_obj]
+
+        raise NotImplementedError('Given webhook cannot be handled yet.')
--- tests/GitHub/test_github_hoster.py
+++ tests/GitHub/test_github_hoster.py
@@ -5,6 +5,12 @@ import vcr
 
 from IGitt.GitHub import GitHubToken
 from IGitt.GitHub.GitHub import GitHub
+from IGitt.GitHub.GitHubComment import GitHubComment
+from IGitt.GitHub.GitHubCommit import GitHubCommit
+from IGitt.GitHub.GitHubIssue import GitHubIssue
+from IGitt.GitHub.GitHubMergeRequest import GitHubMergeRequest
+from IGitt.Interfaces.Actions import IssueActions, MergeRequestActions, \
+    PipelineActions
 
 my_vcr = vcr.VCR(match_on=['method', 'scheme', 'host', 'port', 'path'],
                  filter_query_parameters=['access_token'],
@@ -37,3 +43,57 @@ class TestGitHub(unittest.TestCase):
     def test_get_repo(self):
         self.assertEqual(self.gh.get_repo('gitmate-test-user/test').full_name,
                          'gitmate-test-user/test')
+
+
+class TestGitHubWebhook(unittest.TestCase):
+
+    def setUp(self):
+        self.gh = GitHub(GitHubToken(os.environ.get('GITHUB_TEST_TOKEN', '')))
+        self.default_data = {
+            'repository': {
+                'full_name': 'org/test-repo'
+            },
+            'pull_request': {'number': 0},
+            'issue': {'number': 0},
+            'action': 'opened',
+            'comment': {
+                'id': 1,
+            },
+            'commit': {
+                'sha': 'deadbeef',
+            }
+        }
+
+    def test_unknown_event(self):
+        with self.assertRaises(NotImplementedError):
+            self.gh.handle_webhook('unknown_event', self.default_data)
+
+    def test_issue_hook(self):
+        event, obj = self.gh.handle_webhook('issues', self.default_data)
+        self.assertEqual(event, IssueActions.OPENED)
+        self.assertIsInstance(obj[0], GitHubIssue)
+
+    def test_pr_hook(self):
+        event, obj = self.gh.handle_webhook('pull_request', self.default_data)
+        self.assertEqual(event, MergeRequestActions.OPENED)
+        self.assertIsInstance(obj[0], GitHubMergeRequest)
+
+    def test_issue_comment(self):
+        event, obj = self.gh.handle_webhook('issue_comment', self.default_data)
+        self.assertEqual(event, IssueActions.COMMENTED)
+        self.assertIsInstance(obj[0], GitHubIssue)
+        self.assertIsInstance(obj[1], GitHubComment)
+
+    def test_pr_comment(self):
+        data = self.default_data
+        data['issue']['pull_request'] = {}  # Exists for PRs
+
+        event, obj = self.gh.handle_webhook('issue_comment', data)
+        self.assertEqual(event, MergeRequestActions.COMMENTED)
+        self.assertIsInstance(obj[0], GitHubMergeRequest)
+        self.assertIsInstance(obj[1], GitHubComment)
+
+    def test_status(self):
+        event, obj = self.gh.handle_webhook('status', self.default_data)
+        self.assertEqual(event, PipelineActions.UPDATED)
+        self.assertIsInstance(obj[0], GitHubCommit)
