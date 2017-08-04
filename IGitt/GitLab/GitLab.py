e6199582e7ea3ce0d3fc1cc520ef4c68c7298156
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-08-04 11:35:09 +0200

    GitLab: Add handle_webhook

--- IGitt/GitLab/GitLab.py
+++ IGitt/GitLab/GitLab.py
@@ -5,7 +5,14 @@ Contains the Hoster implementation for GitLab.
 import logging
 
 from IGitt.GitLab import get, GitLabOAuthToken, GitLabPrivateToken
+from IGitt.GitLab.GitLabComment import GitLabComment
+from IGitt.GitLab.GitLabCommit import GitLabCommit
+from IGitt.GitLab.GitLabIssue import GitLabIssue
+from IGitt.GitLab.GitLabMergeRequest import GitLabMergeRequest
 from IGitt.Interfaces import AccessLevel
+from IGitt.Interfaces.Actions import IssueActions, MergeRequestActions, \
+    PipelineActions
+from IGitt.Interfaces.Comment import CommentType
 from IGitt.Interfaces.Hoster import Hoster
 from IGitt.GitLab.GitLabRepository import GitLabRepository
 
@@ -98,3 +105,89 @@ class GitLab(Hoster):
         :return: A repository object.
         """
         return GitLabRepository(self._token, repository)
+
+    def handle_webhook(self, event: str, data: dict):
+        """
+        Handles a GitHub webhook for you.
+
+        If it's an issue event it returns e.g.
+        ``IssueActions.OPENED, [GitLabIssue(...)]``, for comments it returns
+        ``MergeRequestActions.COMMENTED,
+        [GitLabMergeRequest(...), GitLabComment(...)]``.
+
+        :param event: The HTTP_X_GITLAB_EVENT of the request header.
+        :param data:  The pythonified JSON data of the request.
+        :return:      An IssueActions or MergeRequestActions member and a list
+                      of the affected IGitt objects.
+        """
+        repository = (data['project'] if 'project' in data.keys()
+                      else data['object_attributes']['target'])
+
+        if event == 'Issue Hook':
+            issue = data['object_attributes']
+            issue_obj = GitLabIssue(
+                self._token, repository['path_with_namespace'], issue['iid'])
+            trigger_event = {
+                'open': IssueActions.OPENED,
+                'close': IssueActions.CLOSED,
+                'reopen': IssueActions.REOPENED,
+            }.get(data['action'], IssueActions.ATTRIBUTES_CHANGED)
+
+            return trigger_event, [issue_obj]
+
+        if event == 'Merge Request Hook':
+            merge_request_data = data['object_attributes']
+            merge_request_obj = GitLabMergeRequest(
+                self._token,
+                repository['path_with_namespace'],
+                merge_request_data['iid'])
+            trigger_event = {
+                'update': MergeRequestActions.ATTRIBUTES_CHANGED,
+                'open': MergeRequestActions.OPENED,
+                'reopen': MergeRequestActions.REOPENED,
+            }.get(merge_request_data['action'])
+
+            # nasty workaround for finding merge request resync
+            if 'oldrev' in merge_request_data:
+                trigger_event = MergeRequestActions.SYNCHRONIZED
+
+            # no such webhook event action implemented yet
+            if not trigger_event:
+                raise NotImplementedError('Unrecgonized action: ' + event +
+                                          '/' + merge_request_data['action'])
+
+            return trigger_event, [merge_request_obj]
+
+        if event == 'Note Hook':
+            comment = data['object_attributes']
+            comment_type = {
+                'MergeRequest': CommentType.MERGE_REQUEST,
+                'Commit': CommentType.COMMIT,
+                'Issue': CommentType.ISSUE,
+                'Snippet': CommentType.SNIPPET
+            }.get(comment['noteable_type'])
+
+            if comment_type == CommentType.MERGE_REQUEST:
+                iid = data['merge_request']['iid']
+                iss = GitLabMergeRequest(self._token,
+                                         repository['full_name'], iid)
+                action = MergeRequestActions.COMMENTED
+            elif comment_type == CommentType.ISSUE:
+                iid = data['issue']['iid']
+                iss = GitLabIssue(self._token, repository['full_name'], iid)
+                action = IssueActions.COMMENTED
+            else:
+                raise NotImplementedError
+
+            return action, [iss, GitLabComment(
+                self._token, repository['full_name'], iid,
+                comment_type, comment['id']
+            )]
+
+        if event == 'Pipeline Hook':
+            return PipelineActions.UPDATED, [GitLabCommit(
+                self._token,
+                repository['path_with_namespace'],
+                data['commit']['id'])]
+
+        raise NotImplementedError('Given webhook cannot be handled yet.')
--- tests/GitLab/test_gitlab_hoster.py
+++ tests/GitLab/test_gitlab_hoster.py
@@ -5,6 +5,12 @@ import vcr
 
 from IGitt.GitLab import GitLabOAuthToken
 from IGitt.GitLab.GitLab import GitLab
+from IGitt.GitLab.GitLabComment import GitLabComment
+from IGitt.GitLab.GitLabCommit import GitLabCommit
+from IGitt.GitLab.GitLabIssue import GitLabIssue
+from IGitt.GitLab.GitLabMergeRequest import GitLabMergeRequest
+from IGitt.Interfaces.Actions import IssueActions, MergeRequestActions, \
+    PipelineActions
 
 my_vcr = vcr.VCR(match_on=['method', 'scheme', 'host', 'port', 'path'],
                  filter_query_parameters=['access_token'],
@@ -35,3 +41,82 @@ class TestGitLab(unittest.TestCase):
     def test_get_repo(self):
         self.assertEqual(self.gl.get_repo('gitmate-test-user/test').full_name,
                          'gitmate-test-user/test')
+
+
+class TestGitLabWebhook(unittest.TestCase):
+
+    def setUp(self):
+        self.gl = GitLab(GitLabOAuthToken(
+            os.environ.get('GITLAB_TEST_TOKEN', '')))
+        self.default_data = {
+            'project': {
+                'path_with_namespace': 'test/test',
+                'full_name': 'test/test',
+            },
+            'object_attributes': {
+                'id': 12,
+                'iid': 23,
+                'action': 'open',
+                'noteable_type': 'Issue'
+            },
+            'commit': {
+                'id': 'bcbb5ec396a2c0f828686f14fac9b80b780504f2',
+            },
+            'merge_request': {
+                'iid': 123,
+            },
+            'issue': {
+                'iid': 123,
+            },
+            'action': 'open',
+        }
+
+    def test_unknown_event(self):
+        with self.assertRaises(NotImplementedError):
+            self.gl.handle_webhook('unknown_event', self.default_data)
+
+    def test_issue_hook(self):
+        event, obj = self.gl.handle_webhook('Issue Hook', self.default_data)
+        self.assertEqual(event, IssueActions.OPENED)
+        self.assertIsInstance(obj[0], GitLabIssue)
+
+    def test_pr_hook(self):
+        event, obj = self.gl.handle_webhook('Merge Request Hook',
+                                            self.default_data)
+        self.assertEqual(event, MergeRequestActions.OPENED)
+        self.assertIsInstance(obj[0], GitLabMergeRequest)
+
+    def test_pr_synchronized(self):
+        data = self.default_data
+        data['object_attributes']['oldrev'] = 'deadbeef'
+        event, obj = self.gl.handle_webhook('Merge Request Hook',
+                                            self.default_data)
+        self.assertEqual(event, MergeRequestActions.SYNCHRONIZED)
+        self.assertIsInstance(obj[0], GitLabMergeRequest)
+
+    def test_issue_comment(self):
+        event, obj = self.gl.handle_webhook('Note Hook', self.default_data)
+        self.assertEqual(event, IssueActions.COMMENTED)
+        self.assertIsInstance(obj[0], GitLabIssue)
+        self.assertIsInstance(obj[1], GitLabComment)
+
+    def test_unsupported_comment(self):
+        data = self.default_data
+        data['object_attributes']['noteable_type'] = 'Snippet'
+
+        with self.assertRaises(NotImplementedError):
+            self.gl.handle_webhook('Note Hook', data)
+
+    def test_pr_comment(self):
+        data = self.default_data
+        data['object_attributes']['noteable_type'] = 'MergeRequest'
+
+        event, obj = self.gl.handle_webhook('Note Hook', data)
+        self.assertEqual(event, MergeRequestActions.COMMENTED)
+        self.assertIsInstance(obj[0], GitLabMergeRequest)
+        self.assertIsInstance(obj[1], GitLabComment)
+
+    def test_status(self):
+        event, obj = self.gl.handle_webhook('Pipeline Hook', self.default_data)
+        self.assertEqual(event, PipelineActions.UPDATED)
+        self.assertIsInstance(obj[0], GitLabCommit)
