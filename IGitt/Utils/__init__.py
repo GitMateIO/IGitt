981ded116a154f0ceb3dca52e01da9740d66218a
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-08-28 20:57:49 +0200

    Caching: Allow incomplete data

--- IGitt/Utils/__init__.py
+++ IGitt/Utils/__init__.py
@@ -3,6 +3,36 @@ Provides useful stuff, generally!
 """
 
 
+class PossiblyIncompleteDict:
+    """
+    A dict kind of thing (only supporting item getting) that, if an item isn't
+    available, gets fresh data from a refresh function.
+    """
+    def __init__(self, data: dict, refresh):
+        self.may_need_refresh = True
+        self._data = data
+        self._refresh = refresh
+
+    def __getitem__(self, item):
+        if self.may_need_refresh:
+            if item in self._data:
+                return self._data[item]
+
+            self._data = self._refresh()
+            self.may_need_refresh = False
+            return self._data[item]
+        else:
+            return self._data[item]
+
+    def update(self, other):
+        if self.may_need_refresh:
+            self._data = self._refresh()
+            self.may_need_refresh = False
+
+        return self._data.update(other)
+
+
+
 class CachedDataMixin:
     """
     You provide:
@@ -12,6 +42,8 @@ class CachedDataMixin:
     You can also create an IGitt instance with your own data using from_data
     classmethod.
     """
+    default_data = {}
+
     @classmethod
     def from_data(cls, data: dict=frozenset(), *args, **kwargs):
         """
@@ -36,7 +68,8 @@ class CachedDataMixin:
         """
         Refreshes all the data from the hoster!
         """
-        self._data = self._get_data()
+        self._data = PossiblyIncompleteDict(self._get_data(), self._get_data)
+        self._data.may_need_refresh = False
 
     @property
     def data(self):
@@ -44,7 +77,8 @@ class CachedDataMixin:
         Retrieves the data, if needed from the network.
         """
         if not getattr(self, '_data', None):
-            self.refresh()
+            self._data = PossiblyIncompleteDict(
+                self.default_data, self._get_data)
 
         return self._data
 
@@ -53,7 +87,7 @@ class CachedDataMixin:
         """
         Setter for the data, use it to override, refresh, ...
         """
-        self._data = value
+        self._data = PossiblyIncompleteDict(value, self._get_data)
 
 
 def eliminate_none(data):
