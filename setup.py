1f9cf1612d7f8a5af2e36cd7483fa023043bf323
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2018-03-14 03:14:51 +0530

    setup.py: Add docs subcommand

--- setup.py
+++ setup.py
@@ -3,14 +3,32 @@
 """
 Provides a main routine that can install IGitt and create distributions.
 """
+from subprocess import call
+import sys
 
+import setuptools.command.build_py
 from setuptools import find_packages, setup
 
 from IGitt import VERSION
 
+
+class BuildDocsCommand(setuptools.command.build_py.build_py):
+    """
+    Automatically generates the documentation with sphinx.
+    """
+    apidoc_command = (
+        'sphinx-apidoc', '-f', '-o', 'docs', '--no-toc', 'IGitt'
+    )
+    doc_command = ('make', '-C', 'docs', 'html', 'SPHINXOPTS=-W')
+
+    def run(self):
+        errOne = call(self.apidoc_command)
+        errTwo = call(self.doc_command)
+        sys.exit(errOne or errTwo)
+
+
 with open('requirements.txt') as requirements:
     REQUIRED = requirements.read().splitlines()
-
 
 with open('README.rst') as readme:
     long_description = readme.read()
@@ -18,6 +36,7 @@ with open('README.rst') as readme:
 
 if __name__ == '__main__':
     setup(name='IGitt',
+          cmdclass={'docs': BuildDocsCommand},
           version=VERSION,
           description='A git(hub/lab/...) hosting abstraction library.',
           long_description=long_description,
