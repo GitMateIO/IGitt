d5a23eff323aa74558251e7bd51d23a4c1d41ccb
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2018-03-17 15:43:22 +0100

    setup.py: Add more metadata
    Closes https://gitlab.com/gitmate/open-source/IGitt/issues/113

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 """
 Provides a main routine that can install IGitt and create distributions.
 """
+
 from setuptools import find_packages, setup
 
 from IGitt import VERSION
@@ -11,13 +12,19 @@ with open('requirements.txt') as requirements:
     REQUIRED = requirements.read().splitlines()
 
 
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+
 if __name__ == '__main__':
     setup(name='IGitt',
           version=VERSION,
           description='A git(hub/lab/...) hosting abstraction library.',
+          long_description=long_description,
+          url='https://gitlab.com/gitmate/open-source/IGitt/',
           author='Lasse Schuirmann',
           maintainer='Lasse Schuirmann',
-          maintainer_email='lasse.schuirmann@gmail.com',
+          maintainer_email='lasse@gitmate.io',
           packages=find_packages(exclude=['build.*', '*.tests.*', '*.tests']),
           install_requires=REQUIRED,
           package_data={'IGitt': ['VERSION']},
